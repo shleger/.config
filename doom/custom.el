ecf53f2b8c6213d1fda342ad7306d4c43bdadf20
Andrey Sh <Andrey_Shleger@epam.com>
2024-04-19 19:46:31 +0800

    add doom configs

---
+++ doom/custom.el
@@ -0,0 +1,29 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages '(treemacs-all-the-icons dap-mode cmake-mode)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
+(global-set-key (kbd "C-z") 'undo)
+(global-set-key (kbd "C-<tab>") 'centaur-tabs-forward)
+(global-set-key (kbd "C-S-<tab>") 'centaur-tabs-backward)
+
+(global-set-key (kbd "C-S-<return>") 'insert-semicolon-at-the-end )
+(defun insert-semicolon-at-the-end ()
+        "Insert semicolon after the end of the line."
+        (interactive)
+        (move-end-of-line nil)
+        (insert-char #b111011) ;; insert semicolone
+        (newline)
+       )
